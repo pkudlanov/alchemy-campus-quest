5a42d1d73e54f70b1409f89b3fd0c3bd8f4c3e23
Pavel Kudlanov <pkudlanov@yahoo.com>
2019-05-07 00:29:32 -0700

    not sure why travis failed but heres a second shot

--- test/tests.js
+++ test/tests.js
@@ -3,11 +3,10 @@ const test = QUnit.test;
 test('time to test a function', function(assert) {
     //Arrange
     // Set up your parameters and expectations
-
+    const expected = 'Hey';
     //Act 
     // Call the function you're testing and set the result to a const
-
+    const result = 'Hey';
     //Assert
-    assert.equal(true, true);
+    assert.equal(expected, result);
 });
- 

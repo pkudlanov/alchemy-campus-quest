8a4fdf50d953f78fcc1b8b0e57294e7f2a2e0a79
Pavel Kudlanov <pkudlanov@yahoo.com>
2019-05-08 23:40:23 -0700

    make most of the quest metadata

--- src/services/quest-data.js
+++ src/services/quest-data.js
@@ -1,7 +1,7 @@
 const bigClassroom = {
     id: 'bigClassroom',
     title: 'Big Classroom',
-    image:,
+    image: 'image to come',
     description: `
         The class is silent. Everyones eyes are like dagres penetrating
         your inner workings. There's a noise coming from the corner of the
@@ -9,51 +9,87 @@ const bigClassroom = {
         because there isn't a single chalkboard in all of Portland, Oregon.`,
     choices: [{
         id: 'investigate',
-        description: 'You decide to be the good Samaritan and see what is going on.',
+        description: 'You decide to be the good Samaritan and see what is going on',
         result: `
             Turns out it was the canine earthling, gnawing on the metal
             leg of the recliner. As you approach to chase away the animal,
-            the dog starts growling. You tell it to leave which makes it
+            the animal starts growling. You tell it to leave which makes it
             even angrier. It lunges at you, and clings on to your right
             pointer finger. You spill your coffee. All the school faculty is
             angry at you for agitating the thing. After a very serious board
-            meeting it is decided the school will be better off without you.`,
-        grade:,
-        coffee:
+            meeting it is decided the school will be better off without you.
+            Your career is ruined because you don't have an education and
+            now you can't even type anymore from the finger injury.`,
+        grade: -78,
+        coffee: 0
     }, {
-        id:,
-        description:,
-        result:,
-        grade:,
-        coffee:
+        id: 'ignore',
+        description: 'Ignore all the stares and take it like a MAN!!!!',
+        result: `
+            You take all the stares like a man. Let someone else deal with
+            it. You have more important things to do then to get distracted
+            by petty sounds coming from the corner of the room. Some people
+            may be a tad irratated, lowering your grade a tad but you get
+            time to code a sick new program that will compete against the
+            Adobe Creative Suite. Now you also have some free time to load
+            up on coffee. 
+            `,
+        grade: -7,
+        coffee: 4
     }, {
-        id:,
-        description:,
-        result:,
-        grade:,
-        coffee:
+        id: 'getHelp',
+        description: 'Transfer all the attention to your neighbor and ask him to check it out',
+        result: `
+            You\'r closest neighbor seems like a great target to transfer all
+            the attention to. After semi kindly asking them to scout out the
+            disturbance they find that its the local hairy canine friend gnawing
+            at the metal recliner leg. They take care of all the business and
+            now they're a hero. You on the other hand are now a nobody. You
+            do get a raise in your grade for getting someone to help in the
+            situation. You only gain one cup of coffee though so you won't be
+            terribly awake in class and will learn a bit less.
+            `,
+        grade: 17,
+        coffee: 1
     }]
 }
 
-const Kitchen = {
-    id:,
-    title:,
-    image:,
-    description:,
+const kitchen = {
+    id: kitchen,
+    title: 'Kitchen',
+    image: 'image to come',
+    description: `
+        Sitting right there in front of you is one big juicy gronola bar. The
+        nice and chewy one with a bit of crunch from the almonds. It's been a
+        long day of staring at cursor move accross the screen. Blinking occsiionally, 
+        on the dark background when it decides to take a break. Everyone else
+        is preoccupied and has no knowledge of the treasure sitting on the kitchen 
+        table.
+        `,
     choices: [{
-        id:,
+        id: 'eat',
+        description: 'Your tastebuds just couldn\'t resist the treat. You devour it in 7 seconds',
+        result: `
+            In the process you don't even notice how much noise you made with the
+            whole ordeal. Everyone is staring at you now. You even cought the
+            attention of the quite but hungry canine, once friend. Megan comes
+            running in shouting, "Did someone take my granola bar?!" It is decided
+            for such a heinous act you are allowed to stay but the whole teaching
+            crew is now biased against you and every assignment just happens to
+            lose points for the most unorthodox things like a 3 space tab instead
+            of 4 spaces. While you were in the kitchen you did manage to get
+            yourself loaded on 2 cups of coffee though.
+            `,
+        grade: -4,
+        coffee: 2
+    }, {
+        id: 'ask',
         description:,
         result:,
         grade:,
         coffee:
     }, {
-        id:,
-        description:,
-        result:,
-        grade:,
-        coffee:
-    }, {
-        id:,
+        id: 'ignore',
         description:,
         result:,
         grade:,
@@ -64,7 +100,7 @@ const Kitchen = {
 const megansOffice = {
     id:,
     title:,
-    image:,
+    image: 'image to come',
     description:,
     choices: [{
         id:,

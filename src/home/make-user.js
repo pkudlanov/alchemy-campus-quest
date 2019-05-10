b90165434ebfdadc6425411a1507d600f06eaf76
Pavel Kudlanov <pkudlanov@yahoo.com>
2019-05-10 11:51:59 -0700

    populate the profile on map page dynamically

--- src/home/make-user.js
+++ src/home/make-user.js
@@ -3,7 +3,7 @@ function makeUser(formData){
     const user = {
         name: formData.get('name'),
         title: formData.get('title'),
-        grade: 9,
+        grade: 83,
         coffee: 2
     };
     return user;

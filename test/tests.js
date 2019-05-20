e89fe1c470be8bef9a67105c40ed66ca9b0bb406
Pavel Kudlanov <pkudlanov@yahoo.com>
2019-05-19 19:08:44 -0700

    got rid of some extra code in the tests.js

--- test/tests.js
+++ test/tests.js
@@ -1,16 +1,3 @@
 import './make-user.test.js';
 import './api.test.js';
 import './quest-api.test.js';
-
-
-const test = QUnit.test;
-test ('time to test a function', function(assert) {
-    //Arrange
-    // Set up your parameters and expectations
-    const expected = 'Hey';
-    //Act 
-    // Call the function you're testing and set the result to a const
-    const result = 'Hey';
-    //Assert
-    assert.equal(expected, result);
-});
